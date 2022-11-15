bec6ee33d4f82c3b4854a0b297fa1e795acc05cc
Mikhail Golovkov <mik270400@mail.ru>
2022-11-15 22:18:08 +0300

    Addit fail

---
+++ tusk0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b )
+    {
+        Console.WriteLine($"->{a} max ");
+    }   
+else 
+{
+    Console.WriteLine($"->{b} max ");
+}
