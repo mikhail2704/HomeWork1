826de3438a8aa9746dc1a38db45e926aa678ecf7
Mikhail Golovkov <mik270400@mail.ru>
2022-11-15 23:22:45 +0300

    Zadacha4

---
+++ tusk4/Program.cs
@@ -0,0 +1,11 @@
+﻿ Console.Write("Число с которого начнутся отсчёт: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Последнее число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+for (int i = n; i <= m; i++)
+    {
+        if (i % 2 == 0) 
+        {
+            Console.WriteLine("Чётное -> " + i );
+        }
+    }
